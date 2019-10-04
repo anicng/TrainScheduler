0fc23a48590d012142d2015bb79aea4d72cb6dbb
Annie <annie.yiyunc@gmail.com>
2019-10-03 20:09:37 -0700

    added database

---
+++ assets/javascript/app.js
@@ -0,0 +1,37 @@
+  var firebaseConfig = {
+    apiKey: "AIzaSyAnGXSMsz0EnaGllNMGeoz4XVjneCJJzxI",
+    authDomain: "trainschedule-c0c61.firebaseapp.com",
+    databaseURL: "https://trainschedule-c0c61.firebaseio.com",
+    projectId: "trainschedule-c0c61",
+    storageBucket: "trainschedule-c0c61.appspot.com",
+    messagingSenderId: "414807725496",
+    appId: "1:414807725496:web:1e1bd5a9454a5acf0b2b4d"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  var database = firebase.database();
+
+$(document).ready(
+    function trainSchedule() {
+        var trainName
+        var destination
+        var firstTrainTime
+        var frequency
+        
+        // when submit button click
+        $("#submit-button").on("click", function (e) {
+            e.preventDefault();
+            // get the values from input field
+            trainName = $("#train-name").val().trim();
+            console.log(trainName);
+            destination = $("#destination").val().trim();
+            console.log(destination);
+            firstTrainTime = $("#first-train-time").val().trim();
+            console.log(firstTrainTime);
+            frequency = $("frequency").val().trim();
+            console.log(frequency);
+        })
+
+    }
+
+);
